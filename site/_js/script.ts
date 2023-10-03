94d5b63ccea38da818211780e74ed74b3b593164
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-03 23:20:33 +0330

    feat: create fill data

--- site/_js/script.ts
+++ site/_js/script.ts
@@ -1,4 +1,5 @@
 import './banner.js';
+import './data.js';
 import './header.js';
 import './register-service-worker.js';
 import './toolbox.js';
--- site/_js/toolbox.ts
+++ site/_js/toolbox.ts
@@ -1,9 +1,11 @@
-/**
- * Copy button.
- */
+import {generateData} from './data.js';
+
 const copyButton = document.querySelector('.copy-button') as HTMLButtonElement;
 const textarea = document.querySelector('textarea') as HTMLTextAreaElement;
 
+/**
+ * Copy button handler.
+ */
 function copyLorem(): void {
   const value = textarea.value.trim();
   navigator.clipboard.writeText(value);
@@ -12,20 +14,36 @@ function copyLorem(): void {
 copyButton.addEventListener('click', copyLorem);
 
 /**
- * Segmented button.
+ * Segmented button handler.
  */
-function segmentedButtonHandler(buttonList: NodeListOf<HTMLButtonElement>, activeClass: string) {
+function segmentedButtonHandler(buttonList: NodeListOf<HTMLButtonElement>, callback?: () => void | Promise<void>) {
   buttonList.forEach((button) => {
-    button.addEventListener('click', () => {
-      buttonList.forEach((button) => button.classList.remove(activeClass));
-      button.classList.add(activeClass);
+    button.addEventListener('click', async () => {
+      buttonList.forEach((button) => button.classList.remove('active'));
+      button.classList.add('active');
+
+      await callback?.();
     });
   });
 }
 
+/**
+ * Generate data based on the selected length and count.
+ */
+function fillData() {
+  const countButtons = document.querySelector('.count-button-container > button.active') as HTMLButtonElement;
+  const lengthButtons = document.querySelector('.length-button-container > button.active') as HTMLButtonElement;
+
+  const count = countButtons.value;
+  const length = lengthButtons.value as 'word' | 'sentence' | 'paragraph';
+
+  const data = generateData(length, +count);
+
+  textarea.value = data;
+}
+
 const countButtons = document.querySelectorAll('.count-button-container > button') as NodeListOf<HTMLButtonElement>;
 const lengthButtons = document.querySelectorAll('.length-button-container > button') as NodeListOf<HTMLButtonElement>;
-const activeClass = 'active';
 
-segmentedButtonHandler(countButtons, activeClass);
-segmentedButtonHandler(lengthButtons, activeClass);
+segmentedButtonHandler(countButtons, fillData);
+segmentedButtonHandler(lengthButtons, fillData);
