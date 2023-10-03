63d9748f689046933ba649dfdc536200ddae8b07
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-10-03 22:42:29 +0330

    feat: make toolbox dynamic

--- site/_js/toolbox.ts
+++ site/_js/toolbox.ts
@@ -1,3 +1,6 @@
+/**
+ * Copy button.
+ */
 const copyButton = document.querySelector('.copy-button') as HTMLButtonElement;
 const textarea = document.querySelector('textarea') as HTMLTextAreaElement;
 
@@ -7,3 +10,22 @@ function copyLorem(): void {
 }
 
 copyButton.addEventListener('click', copyLorem);
+
+/**
+ * Segmented button.
+ */
+function segmentedButtonHandler(buttonList: NodeListOf<HTMLButtonElement>, activeClass: string) {
+  buttonList.forEach((button) => {
+    button.addEventListener('click', () => {
+      buttonList.forEach((button) => button.classList.remove(activeClass));
+      button.classList.add(activeClass);
+    });
+  });
+}
+
+const countButtons = document.querySelectorAll('.count-button-container > button') as NodeListOf<HTMLButtonElement>;
+const lengthButtons = document.querySelectorAll('.length-button-container > button') as NodeListOf<HTMLButtonElement>;
+const activeClass = 'active';
+
+segmentedButtonHandler(countButtons, activeClass);
+segmentedButtonHandler(lengthButtons, activeClass);
